11eb82e5ac3fd490466223ca23874fb9a5094fd6
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-07 23:55:22 -0300

    feat: Componente personalizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from "next";
 
 function Botao(props) {
-  return <a href="https://github.com/gabrielmds222">{props.address}</a>;
+  return <a href={props.address}>{props.title}</a>;
 }
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Botao address="github" /> <br />
-      <Botao address="google" /> <br />
-      <Botao address="linkedin" /> <br />
+      <Botao title="github" address="https://github.com/gabrielmds222" /> <br />
+      <Botao title="google" address="https://www.google.com.br/" /> <br />
+      <Botao
+        title="linkedin"
+        address="https://www.linkedin.com/mynetwork/"
+      />{" "}
+      <br />
     </div>
   );
 };
