652f9c02dfe9dba2604bb5c0a965868aad887f5c
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-08 00:23:19 -0300

    feat: Cabeçalho da aplicação

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,7 +1,15 @@
-import { CabecalhoContainer } from "./styles";
+import { CabecalhoContainer, Logo } from "./styles";
 
 const Cabecalho = () => {
-  return <CabecalhoContainer>Cabeçalho</CabecalhoContainer>;
+  return (
+    <CabecalhoContainer>
+      <div>
+        <Logo src="/images/myteacher.png" />
+      </div>
+
+      <p> Encontre o professor perfeito! </p>
+    </CabecalhoContainer>
+  );
 };
 
 export default Cabecalho;
