f672c49e2d43370e8334e052fcc94d76832e9675
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-08 00:00:10 -0300

    feat: Instala material ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,7 @@
 import type { NextPage } from "next";
 
-function Botao(props) {
-  return <a href={props.address}>{props.title}</a>;
-}
-
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Botao title="github" address="https://github.com/gabrielmds222" /> <br />
-      <Botao title="google" address="https://www.google.com.br/" /> <br />
-      <Botao
-        title="linkedin"
-        address="https://www.linkedin.com/mynetwork/"
-      />{" "}
-      <br />
-    </div>
-  );
+  return <div>Primeiro projeto</div>;
 };
 
 export default Home;
