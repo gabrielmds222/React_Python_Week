9f1fba291c23d424cf4a6948d2302b1a990a1555
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-07 23:53:18 -0300

    feat: Primeiro componente com Next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
+function Botao(props) {
+  return <a href="https://github.com/gabrielmds222">{props.address}</a>;
+}
+
 const Home: NextPage = () => {
-  return <div>Primeiro projeto</div>;
+  return (
+    <div>
+      <Botao address="github" /> <br />
+      <Botao address="google" /> <br />
+      <Botao address="linkedin" /> <br />
+    </div>
+  );
 };
 
 export default Home;
