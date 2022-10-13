f75726e7414f3cf8328abc013a42942345bdb533
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-13 04:48:27 -0300

    feat: Cria service para conectar e consumir api

--- src/hooks/pages/useIndex.ts
+++ src/hooks/pages/useIndex.ts
@@ -1,37 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Professor } from "../../@types/professor";
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex() {
-  const [listaProfessores, setListaProfessores] = useState<Professor[]>([
-    {
-      id: 1,
-      nome: "Professor 1",
-      foto: "https://github.com/gabrielmds222.png",
-      descricao: "Professor de front-end",
-      valor_hora: 100,
-    },
-    {
-      id: 2,
-      nome: "Professor 2",
-      foto: "https://github.com/gabrielmds222.png",
-      descricao: "Professor de front-end",
-      valor_hora: 100,
-    },
-    {
-      id: 3,
-      nome: "Professor 1",
-      foto: "https://github.com/gabrielmds222.png",
-      descricao: "Professor de front-end",
-      valor_hora: 100,
-    },
-    {
-      id: 4,
-      nome: "Professor 1",
-      foto: "https://github.com/gabrielmds222.png",
-      descricao: "Professor de front-end",
-      valor_hora: 100,
-    },
-  ]);
+  const [listaProfessores, setListaProfessores] = useState<Professor[]>([]);
+
+  useEffect(() => {
+    ApiService.get("/professores/").then((response) => {
+      setListaProfessores(response.data);
+    });
+  }, []);
 
   return {
     listaProfessores,
